3ce73358d92cc2c5a9f5458fd0547c081c3fa282
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-11-11 10:20:40 -0700

    Update file-editor.sh

--- Server-Scrips/other/file-editor.sh
+++ Server-Scrips/other/file-editor.sh
@@ -1,12 +1,12 @@
 cd ~/MineStart/MC-Servers
 printf "\u001b[34mWhat Server Do You Want To Edit?\u001b[0m";echo;
-ls
+ls;
 read MCfile;
 cd ~/MineStart/MC-Servers/$MCfile;
 printf "\u001b[34mWhich File Do You Want To Edit?\u001b[0m";echo;
-ls
+ls;
 read filename;
-nano $filename
+nano $filename;
 printf "\u001b[34mIs That It?\u001b[0m(y/n)";echo;
 read next;
 case $next in
