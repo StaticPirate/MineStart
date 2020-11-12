6788a811361cb13b990ec44a5236ea567a787720
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-11-12 12:29:23 -0700

    bug fix

--- Server-Scrips/minemc.sh
+++ Server-Scrips/minemc.sh
@@ -5,7 +5,7 @@ printf "\u001b[34mWhat Was The Name Of Your Server?\u001b[0m";echo;
 ls;
 read chname;
 cd ~/MineStart/MC-Servers/$chname;
-printf "\u001b[34mHow Much Ram Do You Want To Give Your Minecraft Server? IN Mb";echo;
+printf "\u001b[34mHow Much Ram Do You Want To Give Your Minecraft Server? IN Mb\u001b[0m";echo;
 read ram;
 echo java -Xmx"$ram"M -Xms"$ram"M -jar server.jar nogui > Start.sh
 chmod 755 Start.sh
