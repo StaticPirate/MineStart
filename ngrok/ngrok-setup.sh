4be077edd7ce44a5393648d4afc7f6bff047cf4d
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-11-19 10:57:09 -0700

    bug fix

--- ngrok/ngrok-setup.sh
+++ ngrok/ngrok-setup.sh
@@ -1,11 +1,12 @@
 cc='\033[0m' #No Color
 green='\033[0;32m'
 
-cd ~/MineStart/ngrok
+cd ~/MineStart/ngrok;
 printf "${green}Sign Up For A ngrok Account (https://ngrok.com/)${cc}";echo;
 echo;
-printf "${green}Enter Your Authtoken${cc}"
+printf "${green}Enter Your Authtoken ${cc}";
 read authtoken;
 ./ngrok authtoken $authtoken;
-printf "${greem}[+]Done!${cc}"
-
+printf "${green}[+]Done!${cc}";echo;
+cd ~/MineStart;
+./MineStart.sh
